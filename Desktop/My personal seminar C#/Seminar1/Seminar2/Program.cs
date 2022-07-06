027285a95fc37e1c469c3f0e15026355e6eef826
RomanBelykh <romanfizikh50@gmail.com>
2022-07-06 20:58:04 +0300

    Задачи по второму семинару

---
+++ Desktop/My personal seminar C#/Seminar1/Seminar2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 10
+/*int ShowSecondNumber()
+ {
+    int rand = new Random().Next(100,1000);
+    Console.WriteLine("Second number is " + rand);
+
+    
+    int secondnumber = rand % 100;
+    
+    int result = secondnumber/10;
+    return result;
+ }
+
+ int res = ShowSecondNumber();
+ Console.WriteLine("Полученная цифра " + res);
+
+ */
+
+//Задача 13
+/*Console.Write("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(num);
+
+if (str.Length > 2)
+    {
+        Console.WriteLine("Третья цифра - " + str[2]);
+    }
+else 
+    {
+        Console.WriteLine(" - третья цифра отсутствует");
+    }
+    */
+   
+
+//Задача 15
+/*void day(int a)
+{
+     if(a > 7)
+     {
+     Console.WriteLine(" В неделе только 7 дней ");
+     }
+
+     if (a > 0 && a < 6)
+     {
+     Console.WriteLine(" -> это будний день :( ");
+     }
+
+     if (a > 5 && a < 8)
+     {
+     Console.WriteLine("-> это выходной :) ");
+     }
+}
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+day(a);
+*/
